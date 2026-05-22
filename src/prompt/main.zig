0563db455cb3d6bded22d7d77669a1eddbf10dcc
Parker Anderson <parker.anderson@firemon.com>
2026-05-21 19:52:24 -0500

    feat(prompt): segment model

--- src/prompt/main.zig
+++ src/prompt/main.zig
@@ -3,8 +3,10 @@
 
 const std = @import("std");
 const icons = @import("icons.zig");
+const segments = @import("segments.zig");
 
 pub fn main() void {
     _ = icons;
+    _ = segments;
     std.debug.print("caldera-prompt\n", .{});
 }
